1765be29c7dee31802bbfb020bfd048972794740
satvik1002 <satvikkaushal8@gmail.com>
2019-12-03 19:45:07 +0530

    First Commit

---
+++ MessageSender/src/main/java/com/example/demo/model/Places.java
@@ -0,0 +1,48 @@
+package com.example.demo.model;
+
+public class Places {
+	
+	
+	private String id;
+	private String city;
+	private String country;
+	
+	
+	public Places( String id, String city, String country) {
+		
+		
+		this.id = id;
+		this.city = city;
+		this.country = country;
+	}
+    
+	
+
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public String getCity() {
+		return city;
+	}
+
+	public void setCity(String city) {
+		this.city = city;
+	}
+
+	public String getCountry() {
+		return country;
+	}
+
+	public void setCountry(String country) {
+		this.country = country;
+	}
+	
+	
+	
+
+}
